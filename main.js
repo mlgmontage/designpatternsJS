0b132fbd8b195ffb15ce13a669a2845a1d95a9dd
mlgmontage <kettleofrassel1@gmail.com>
2020-04-25 22:31:55 -0700

    strategy pattern

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-console.log("hello world");
+// Strategy pattern
+
+class Commute {
+  travel(transport) {
+    return transport.travelTime();
+  }
+}
+
+class Vehicle {
+  travelTime() {
+    return this._timeTaken;
+  }
+}
+
+// Strategy 1
+class Bus extends Vehicle {
+  constructor() {
+    super();
+    this._timeTaken = 10;
+  }
+}
+
+// Strategy 2
+class Taxi extends Vehicle {
+  constructor() {
+    super();
+    this._timeTaken = 5;
+  }
+}
+
+const commute = new Commute();
+console.log(commute.travel(new Taxi()));
+console.log(commute.travel(new Bus()));
