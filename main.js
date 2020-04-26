09cc4f92d73984cc8876c441cb22ca79fbd1e543
mlgmontage <kettleofrassel1@gmail.com>
2020-04-25 22:16:59 -0700

    state pattern

--- main.js
+++ main.js
@@ -1,1 +1,79 @@
-console.log("hello world");
+// State pattern
+
+class Light {
+  constructor(light) {
+    this.light = light;
+  }
+}
+
+class GreenLight extends Light {
+  constructor() {
+    super("green");
+  }
+
+  sign() {
+    return "GO";
+  }
+}
+
+class YellowLight extends Light {
+  constructor() {
+    super("yellow");
+  }
+
+  sign() {
+    return "STEADY";
+  }
+}
+
+class RedLight extends Light {
+  constructor() {
+    super("red");
+  }
+
+  sign() {
+    return "STOP";
+  }
+}
+
+class TrafficLight {
+  constructor() {
+    this._states = [new GreenLight(), new YellowLight(), new RedLight()];
+    this.state = this._states[0];
+  }
+
+  change() {
+    const totalStates = this._states.length;
+    const currentIndex = this._states.findIndex(
+      (light) => light === this.state
+    );
+    if (currentIndex + 1 < totalStates) {
+      this.state = this._states[currentIndex + 1];
+    } else {
+      this.state = this._states[0];
+    }
+  }
+
+  sign() {
+    return this.state.sign();
+  }
+}
+
+const trafficLight = new TrafficLight();
+
+console.log(trafficLight.sign());
+trafficLight.change();
+
+console.log(trafficLight.sign());
+trafficLight.change();
+
+console.log(trafficLight.sign());
+trafficLight.change();
+
+console.log(trafficLight.sign());
+trafficLight.change();
+
+console.log(trafficLight.sign());
+trafficLight.change();
+
+console.log(trafficLight.sign());
