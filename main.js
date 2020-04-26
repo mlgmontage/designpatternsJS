96d9d1eee62257efb0aed56bf6b0b3b1ef7f03ee
mlgmontage <kettleofrassel1@gmail.com>
2020-04-25 22:43:16 -0700

    template pattern

--- main.js
+++ main.js
@@ -1,1 +1,44 @@
-console.log("hello world");
+// Template pattern
+
+class Employee {
+  constructor(name, salary) {
+    this._name = name;
+    this._salary = salary;
+  }
+
+  work() {
+    return `${this._name} handles ${this.responsiblities()}`;
+  }
+
+  paid() {
+    return `${this._name} getting paid ${this._salary}`;
+  }
+}
+
+class Developoer extends Employee {
+  constructor(name, salary) {
+    super(name, salary);
+  }
+
+  responsiblities() {
+    return "Development";
+  }
+}
+
+class Tester extends Employee {
+  constructor(name, salary) {
+    super(name, salary);
+  }
+
+  responsiblities() {
+    return "Testing";
+  }
+}
+
+const dev = new Developoer("Joshua", 2000);
+console.log(dev.work());
+console.log(dev.paid());
+
+const tester = new Tester("Steve", 10000);
+console.log(tester.work());
+console.log(tester.paid());
